b5a52b95996de1115ebfd39c78478759879bebc1
JuliaSantanaV <0001142875@senaimgaluno.com.br>
2025-11-25 17:24:26 -0300

    first commit

---
+++ aula3/src/Components/Page2.jsx
@@ -0,0 +1,11 @@
+import '/src/Css/page2.css'
+function Page2() {
+  return (
+    <div className="page2">
+      <h1>Bem-vindo a Página2</h1>
+      <p>Estilização Local</p>
+    </div>
+  )
+}
+
+export default Page2
---
+++ aula3/src/Components/Page3.jsx
@@ -0,0 +1,11 @@
+import '/src/Css/page3.css'
+function Page3() {
+  return (
+    <div className="page3">
+      <h1>Bem-vindo a Página3</h1>
+      <p >Estilização Local</p>
+    </div>
+  )
+}
+
+export default Page3
